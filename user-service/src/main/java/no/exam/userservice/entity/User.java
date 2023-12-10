a95f212ad979fe125ead3e3f4dc9be57cc63a9ed
Krissapter <kevenrud@hotmail.no>
2023-12-10 15:14:37 +0100

    Set up Users and prepare for pull

---
+++ user-service/src/main/java/no/exam/userservice/entity/User.java
@@ -0,0 +1,10 @@
+package no.exam.userservice.entity;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
