4a6eeb765b41b749b52a7d72e048045b748ea267
Krissapter <kevenrud@hotmail.no>
2023-12-10 15:46:47 +0100

    Created foundations for user class

---
+++ user-service/src/main/java/no/exam/userservice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package no.exam.userservice.repository;
+
+import no.exam.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
